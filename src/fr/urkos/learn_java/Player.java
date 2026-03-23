1cb416066484991510854adc4b620c00507e61f3
Yannickthomas0602 <yannick.thomas@supdevinci-edu.fr>
2026-03-23 17:55:54 +0100

    création d'un objet player avec des methodes de bases

--- src/fr/urkos/learn_java/Player.java
+++ src/fr/urkos/learn_java/Player.java
@@ -1,45 +1,76 @@
 package fr.urkos.learn_java;
 
+import java.util.Arrays;
+
 public class Player {
 
     private String name;
-    private double health;
-    private double attack;
+    private int health = 100;
+    private double attack = 1.0;
+    private int bois = 0;
+    private int pierre = 0;
+    private int metal = 0;
+    private int baton = 0;
+    private String[] objets = {};
 
-    public Player(String name, double health, double attack) {
+    public Player(String name) {
         this.name = name;
-        this.health = health;
-        this.attack = attack;
 
     }
 
-    public String getName() {
-        return name;
+    public void recolte_arbre () {
+        int bois_recolte = (int) (Math.random()*5) + 1 ;
+        int baton_recolte = (int) (Math.random()*3);
+
+        bois += bois_recolte;
+        baton += baton_recolte;
+
+    }
+
+    public void recolte_roche() {
+        int pierre_recolte = (int) (Math.random()+4) +1;
+        int metal_recolte = (int) (Math.random()*3);
+
+        pierre += pierre_recolte;
+        metal += metal_recolte;
+    }
+
+    public void fabrique_baton() {
+        baton += 4;
+        bois -= 1;
     }
 
     public void setName(String name) {
         this.name = name;
     }
 
-    public double getHealth() {
-        return health;
+    public String getName() {
+        return name;
     }
 
-    public void setHealth(double health) {
-        this.health = health;
+    public int getHealth() {
+        return health;
     }
 
     public double getAttack() {
         return attack;
     }
 
-    public void setAttack(double attack) {
-        this.attack = attack;
+    public int getBois() {
+        return bois;
     }
 
-    public void damage(double damage) {
-        this.health -= damage;
-        System.out.println(name + " viens de subir " + damage + " dégats");
+    public int getPierre() {
+        return pierre;
     }
+
+    public int getMetal() {
+        return metal;
+    }
+
+    public int getBaton() {
+        return baton;
+    }
+
 
 }
